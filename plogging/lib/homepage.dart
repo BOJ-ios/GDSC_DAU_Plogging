de98e2d9e37fbebf9107b11efa766f92e4f10048
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-11-09 02:25:45 +0900

    회원가입 조건추가, 사진선택업로드, 사진찍기
    회원가입 조건(학교선택, 이름 추가입력)
    사진선택업로드(사진찍고 업로드 X, 디바이스에서 사진선택 업로드 O)
    사진찍기(인앱 카메라 화면은 안보이는데 찍고 업로드 가능)

--- plogging/lib/homepage.dart
+++ plogging/lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:plogging/camerapage.dart';
 import 'package:plogging/loginpage.dart';
 import 'package:provider/provider.dart';
 import 'package:plogging/mappage.dart';
@@ -7,6 +8,7 @@ import 'package:plogging/pedometer.dart';
 import 'package:plogging/upload_state.dart';
 
 import 'auth_service.dart';
+import 'package:plogging/camera.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -36,6 +38,7 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(user != null ? '환영합니다, ${user.email}' : '로그인 상태가 아닙니다.'),
+            Text(user != null ? 'UID, ${user.uid}' : '로그인 상태가 아닙니다.'),
             const SizedBox(height: 20), // 공간을 띄워줍니다.
             ElevatedButton(
               onPressed: () {
@@ -57,17 +60,41 @@ class HomePage extends StatelessWidget {
               child: const Text('만보계 보기'),
             ),
             const SizedBox(height: 20),
-            Consumer<UploadState>( // Consumer 위젯을 사용하여 UploadState의 상태를 가져옵니다.
-              builder: (context, uploadState, child) {
-                return ElevatedButton(
-                  onPressed: () {
-                    // UploadState의 현재 상태를 가져와서 반대로 설정
-                    bool currentStatus = uploadState.isUploaded;
-                    uploadState.setUploadStatus(!currentStatus);
-                  },
-                  child: Text('상태 변경 (현재: ${uploadState.isUploaded ? "true" : "false"})'), // 버튼의 텍스트를 동적으로 변경합니다.
+            // 카메라 페이지로 가는 버튼 추가
+            ElevatedButton(
+              onPressed: () {
+                // CameraPage로 이동
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) =>
+                      const CameraExample(), // CameraPage 클래스를 생성해야 함
+                ));
+              },
+              child: const Text('사진선택 업로드'),
+            ),
+            const SizedBox(height: 20), // 공간을 띄워줍니다.
+            // 카메라 페이지로 가는 버튼 추가
+            ElevatedButton(
+              onPressed: () {
+                // CameraPage로 이동
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) =>
+                      const CameraPage(), // CameraPage 클래스를 생성해야 함
+                ));
+              },
+              child: const Text('카메라 촬영'),
+            ),
+            const SizedBox(height: 50), // 공간을 띄워줍니다.
+            ElevatedButton(
+              onPressed: () {
+                // 탈퇴 메서드를 호출
+                Provider.of<AuthService>(context, listen: false)
+                    .deleteAccount();
+                // 로그인 페이지로 이동
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
                 );
               },
+              child: const Text('바로 탈퇴'),
             ),
           ],
         ),
