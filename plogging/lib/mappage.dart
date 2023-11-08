1a51c845876c35be621f754754eb3d5ec4f53088
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-11-08 11:11:25 +0900

    api 키 local.properties에서 받아오는 코드 일부 수정

--- plogging/lib/mappage.dart
+++ plogging/lib/mappage.dart
@@ -3,13 +3,16 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 
 class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
   @override
   _MapPageState createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
-  GoogleMapController? mapController;
+  late GoogleMapController mapController;
   Position? currentPosition;
+  bool isLocationPermissionGranted = false;
 
   @override
   void initState() {
@@ -36,39 +39,46 @@ class _MapPageState extends State<MapPage> {
     }
 
     if (permission == LocationPermission.deniedForever) {
-      throw Exception('Location permissions are permanently denied, we cannot request permissions.');
+      throw Exception(
+          'Location permissions are permanently denied, we cannot request permissions.');
     }
 
     position = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high,
     );
 
+    // 권한 확인 후 상태를 업데이트합니다.
     setState(() {
       currentPosition = position;
+      isLocationPermissionGranted = true; // 권한이 부여되었음을 표시합니다.
     });
 
-    mapController?.animateCamera(CameraUpdate.newCameraPosition(
-      CameraPosition(
-        target: LatLng(position.latitude, position.longitude),
-        zoom: 15.0,
-      ),
-    ));
+    // 위치 권한이 있는 경우에만 카메라를 현재 위치로 이동합니다.
+    if (isLocationPermissionGranted) {
+      mapController.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 15.0,
+        ),
+      ));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Google Maps'),
+        title: const Text('Google Maps'),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.location_on),
+            icon: const Icon(Icons.location_on),
             onPressed: () {
               // 버튼을 누르면 현재 위치로 이동하는 코드를 추가하세요.
               if (currentPosition != null) {
-                mapController?.animateCamera(CameraUpdate.newCameraPosition(
+                mapController.animateCamera(CameraUpdate.newCameraPosition(
                   CameraPosition(
-                    target: LatLng(currentPosition!.latitude, currentPosition!.longitude),
+                    target: LatLng(
+                        currentPosition!.latitude, currentPosition!.longitude),
                     zoom: 15.0,
                   ),
                 ));
@@ -81,7 +91,7 @@ class _MapPageState extends State<MapPage> {
         onMapCreated: (GoogleMapController controller) {
           mapController = controller;
         },
-        initialCameraPosition: CameraPosition(
+        initialCameraPosition: const CameraPosition(
           target: LatLng(0, 0),
           zoom: 10,
         ),
