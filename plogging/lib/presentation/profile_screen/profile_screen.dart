19bc6ddffa6f94f9dd1555b8ae7dce9d3a9b6a73
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-11-10 05:36:40 +0900

    프로필 페이지
    점수표시 제외

--- plogging/lib/presentation/profile_screen/profile_screen.dart
+++ plogging/lib/presentation/profile_screen/profile_screen.dart
@@ -1,29 +1,170 @@
-import 'package:plogging/core/app_export.dart';
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:plogging/camerapage.dart';
-import 'package:plogging/loginpage.dart';
+import 'package:plogging/core/app_export.dart';
+import 'package:plogging/auth_service.dart';
+import 'package:plogging/presentation/main_screen/main_screen.dart';
 import 'package:plogging/presentation/log_in_screen/log_in_screen.dart';
 import 'package:provider/provider.dart';
-import 'package:plogging/mappage.dart';
-import 'package:plogging/pedometer.dart';
-import 'package:plogging/auth_service.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  late Future<int> _markersCountFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _markersCountFuture = _calculateMarkersCount();
+  }
+
+  Future<int> _calculateMarkersCount() async {
+    final String userId = FirebaseAuth.instance.currentUser!.uid;
+    final QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .collection('markers')
+        .get();
+    return querySnapshot.size;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    User? user = Provider.of<AuthService>(context).currentUser(); // 현재 사용자 가져오기
-    mediaQueryData = MediaQuery.of(context);
-    return SafeArea(
-        child: Scaffold(
-            body: SizedBox(
-                width: double.maxFinite,
-                child: Column(children: [
-                  IconButton(
-                    icon: const Icon(Icons.logout),
+    User? user = Provider.of<AuthService>(context).currentUser();
+    return FutureBuilder<DocumentSnapshot>(
+      future:
+          FirebaseFirestore.instance.collection('users').doc(user?.uid).get(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done &&
+            snapshot.hasData) {
+          Map<String, dynamic> userData =
+              snapshot.data!.data() as Map<String, dynamic>;
+          String userName = userData['name'] ?? 'No Name';
+          String userEmail = userData['email'] ?? 'No Email';
+          String userSchool = userData['schoolName'] ?? 'No School';
+
+          return FutureBuilder<int>(
+            future: _markersCountFuture,
+            builder: (context, markersSnapshot) {
+              int markersCount = markersSnapshot.data ?? 0;
+
+              return Scaffold(
+                body: Column(
+                  children: [
+                    Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 80.h, vertical: 24.v),
+                        decoration: AppDecoration.gradientIndigoAToIndigoA
+                            .copyWith(
+                                borderRadius:
+                                    BorderRadiusStyle.customBorderBL50),
+                        child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              SizedBox(height: 60.v),
+                              CustomImageView(
+                                  imagePath: ImageConstant.imgRectangle1565,
+                                  height: 141.adaptSize,
+                                  width: 141.adaptSize,
+                                  radius: BorderRadius.circular(70.h),
+                                  alignment: Alignment.center),
+                              SizedBox(height: 15.v),
+                              Text(userName,
+                                  style: theme.textTheme.headlineSmall),
+                              SizedBox(height: 15.v),
+                              Text(userEmail,
+                                  style: theme.textTheme.labelMedium),
+                              SizedBox(height: 10.v),
+                              Text(userSchool,
+                                  style: theme.textTheme.labelMedium)
+                            ])),
+                    const SizedBox(height: 40),
+                    Padding(
+                      padding: EdgeInsets.only(left: 80.h, right: 77.h),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Text("Markers",
+                                  style: theme.textTheme.bodyMedium),
+                              Text("$markersCount",
+                                  style: CustomTextStyles
+                                      .titleMediumRobotoBlueA200),
+                            ],
+                          ),
+                          Column(children: [
+                            Text("Total Point",
+                                style: theme.textTheme.bodyMedium),
+                            Text("1000 P",
+                                style:
+                                    CustomTextStyles.titleMediumRobotoBlueA200)
+                          ]),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 16.v),
+                    Divider(indent: 49.h, endIndent: 50.h),
+                    SizedBox(height: 25.v),
+                    CustomImageView(
+                        imagePath: ImageConstant.imgSaly18,
+                        height: 147.v,
+                        width: 157.h),
+                    SizedBox(height: 40.v),
+                  ],
+                ),
+                bottomNavigationBar: Container(
+                  padding: EdgeInsets.symmetric(vertical: 15.v),
+                  decoration: BoxDecoration(
+                    color: Colors.white, // 바탕색 설정
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 0,
+                        blurRadius: 10,
+                        offset:
+                            const Offset(0, -1), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CustomImageView(
+                        imagePath: ImageConstant.imgGroup952,
+                        height: 24.v,
+                        width: 24.h,
+                        onTap: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) => const MainScreen()),
+                          );
+                        },
+                      ),
+                      CustomImageView(
+                        imagePath: ImageConstant.imgMapPin,
+                        height: 27.adaptSize,
+                        width: 27.adaptSize,
+                        onTap: () {
+                          Navigator.pushNamed(context, AppRoutes.mapScreen);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                floatingActionButton: Padding(
+                  padding: EdgeInsets.only(top: 8.h), // 상단 패딩 조정
+                  child: FloatingActionButton(
+                    backgroundColor: Colors.white,
+                    elevation: 2,
                     onPressed: () {
                       // 로그아웃 메서드를 호출
                       Provider.of<AuthService>(context, listen: false)
@@ -33,111 +174,21 @@ class ProfileScreen extends StatelessWidget {
                         MaterialPageRoute(builder: (context) => LogInScreen()),
                       );
                     },
+                    child: const Icon(Icons.logout, color: Colors.black),
                   ),
-                  _buildOne(context),
-                  SizedBox(height: 40.v),
-                  Padding(
-                      padding: EdgeInsets.only(left: 80.h, right: 77.h),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(children: [
-                              Text("Photo", style: theme.textTheme.bodyMedium),
-                              Text("50",
-                                  style: CustomTextStyles
-                                      .titleMediumRobotoBlueA200)
-                            ]),
-                            CustomImageView(
-                                imagePath: ImageConstant.imgQrcode,
-                                height: 32.v,
-                                width: 73.h,
-                                margin: EdgeInsets.only(top: 2.v, bottom: 3.v))
-                          ])),
-                  SizedBox(height: 16.v),
-                  Divider(indent: 49.h, endIndent: 50.h),
-                  SizedBox(height: 23.v),
-                  CustomImageView(
-                      imagePath: ImageConstant.imgSaly18,
-                      height: 147.v,
-                      width: 157.h),
-                  SizedBox(height: 5.v)
-                ])),
-            bottomNavigationBar: _buildSixteen(context)));
-  }
+                ),
+                floatingActionButtonLocation:
+                    FloatingActionButtonLocation.miniStartTop,
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
 
-  /// Section Widget
-  Widget _buildOne(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.symmetric(horizontal: 102.h, vertical: 24.v),
-        decoration: AppDecoration.gradientIndigoAToIndigoA
-            .copyWith(borderRadius: BorderRadiusStyle.customBorderBL50),
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              SizedBox(height: 29.v),
-              CustomImageView(
-                  imagePath: ImageConstant.imgRectangle1565,
-                  height: 141.adaptSize,
-                  width: 141.adaptSize,
-                  radius: BorderRadius.circular(70.h),
-                  alignment: Alignment.center),
-              SizedBox(height: 9.v),
-              Text("KIM MINGYU", style: theme.textTheme.headlineSmall),
-              Padding(
-                  padding: EdgeInsets.only(left: 24.h),
-                  child: Text("noreply@github.com",
-                      style: theme.textTheme.labelMedium)),
-              Padding(
-                  padding: EdgeInsets.only(left: 28.h),
-                  child: Text("Donga-A University",
-                      style: theme.textTheme.labelMedium))
-            ]));
-  }
-
-  /// Section Widget
-  Widget _buildSixteen(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.only(left: 69.h, right: 69.h, bottom: 32.v),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          CustomImageView(
-              imagePath: ImageConstant.imgHeart,
-              height: 24.adaptSize,
-              width: 24.adaptSize,
-              onTap: () {
-                onTapImgImage(context);
-              }),
-          CustomImageView(
-              imagePath: ImageConstant.imgGroup952,
-              height: 22.v,
-              width: 14.h,
-              onTap: () {
-                onTapImgImage1(context);
-              }),
-          CustomImageView(
-              imagePath: ImageConstant.imgMapPin,
-              height: 24.adaptSize,
-              width: 24.adaptSize,
-              onTap: () {
-                onTapImgMapPin(context);
-              })
-        ]));
-  }
-
-  /// Navigates to the cameraScreen when the action is triggered.
-  onTapImgImage(BuildContext context) {
-    Navigator.pushNamed(context, AppRoutes.cameraScreen);
-  }
-
-  /// Navigates to the mainScreen when the action is triggered.
-  onTapImgImage1(BuildContext context) {
-    Navigator.pushNamed(context, AppRoutes.mainScreen);
-  }
-
-  /// Navigates to the mapScreen when the action is triggered.
-  onTapImgMapPin(BuildContext context) {
-    Navigator.pushNamed(context, AppRoutes.mapScreen);
+        // If the connection to the Firestore is still ongoing, show a loader
+        return const Center(child: CircularProgressIndicator());
+      },
+    );
   }
 }
