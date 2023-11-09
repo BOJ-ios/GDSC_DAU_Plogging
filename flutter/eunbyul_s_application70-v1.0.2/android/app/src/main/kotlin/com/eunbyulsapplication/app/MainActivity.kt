1b9ba425c0b12cc4a440942f964bbd13666a521e
하은지 <eunbyul0805@gmail.com>
2023-11-10 02:08:31 +0900

    figma -> flutter

---
+++ flutter/eunbyul_s_application70-v1.0.2/android/app/src/main/kotlin/com/eunbyulsapplication/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eunbyulsapplication.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/eunbyul_s_application70-v1.0.2/lib/presentation/get_started_one_screen/get_started_one_screen.dart
@@ -0,0 +1,81 @@
+import 'package:eunbyul_s_application70/core/app_export.dart';
+import 'package:eunbyul_s_application70/widgets/custom_elevated_button.dart';
+import 'package:flutter/material.dart';
+
+class GetStartedOneScreen extends StatelessWidget {
+  const GetStartedOneScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    mediaQueryData = MediaQuery.of(context);
+    return SafeArea(
+        child: Scaffold(
+            body: SizedBox(
+                height: 549.v,
+                width: double.maxFinite,
+                child: Stack(alignment: Alignment.bottomCenter, children: [
+                  Align(
+                      alignment: Alignment.topCenter,
+                      child: SizedBox(
+                          height: 486.v,
+                          width: double.maxFinite,
+                          child:
+                              Stack(alignment: Alignment.topCenter, children: [
+                            CustomImageView(
+                                imagePath: ImageConstant.imgEllipse1486x360,
+                                height: 486.v,
+                                width: 360.h,
+                                alignment: Alignment.center),
+                            _buildTwentyFour(context)
+                          ]))),
+                  CustomImageView(
+                      imagePath: ImageConstant.imgSaly32,
+                      height: 477.v,
+                      width: 360.h,
+                      alignment: Alignment.bottomCenter)
+                ])),
+            bottomNavigationBar: _buildSignIn(context)));
+  }
+
+  /// Section Widget
+  Widget _buildTwentyFour(BuildContext context) {
+    return Align(
+        alignment: Alignment.topCenter,
+        child: Padding(
+            padding: EdgeInsets.only(left: 17.h, top: 58.v, right: 20.h),
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Congratulations",
+                      style: CustomTextStyles.displayMediumBlack900),
+                  SizedBox(height: 2.v),
+                  SizedBox(
+                      width: 210.h,
+                      child: Text(
+                          "Your subscription has been \nsuccessfully completed !",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: CustomTextStyles.titleMediumBlack900)),
+                  SizedBox(height: 13.v),
+                  Text("Please click “Sign in” to log in",
+                      style: CustomTextStyles.titleMediumBlack900)
+                ])));
+  }
+
+  /// Section Widget
+  Widget _buildSignIn(BuildContext context) {
+    return CustomElevatedButton(
+        width: 143.h,
+        text: "Sign in",
+        margin: EdgeInsets.only(left: 108.h, right: 109.h, bottom: 45.v),
+        onPressed: () {
+          onTapSignIn(context);
+        });
+  }
+
+  /// Navigates to the logInScreen when the action is triggered.
+  onTapSignIn(BuildContext context) {
+    Navigator.pushNamed(context, AppRoutes.logInScreen);
+  }
+}
